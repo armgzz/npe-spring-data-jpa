ecf5a46dbefbe26e27219793c3c54ce9ebaf2dad
Armida González González <armida.gonzalez@banregio.com>
2021-04-17 13:34:45 -0500

    Primer commit

---
+++ src/main/java/com/armgzz/npespringdatajpa/npespringdatajpa/NpeSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.armgzz.npespringdatajpa.npespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpeSpringDataJpaApplication.class, args);
+	}
+
+}
